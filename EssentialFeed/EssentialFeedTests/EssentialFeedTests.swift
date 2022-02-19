33d45811fd5ba742a121cd73041f636b50277936
Dan Smith <dsmith42@tutanota.com>
2022-02-19 10:55:11 +0000

    Setup empty project

---
+++ EssentialFeed/EssentialFeedTests/EssentialFeedTests.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeedTests.swift
+//  EssentialFeedTests
+//
+//  Created by Dan Smith on 19/02/2022.
+//
+
+import XCTest
+@testable import EssentialFeed
+
+class EssentialFeedTests: XCTestCase {
+
+    func test_zero() {
+//        XCTFail("Tests not yet implemented")
+    }
+
+}
