311dfe6b2a2c8a1a15bab405b988382a5d4aaea1
Dan Smith <dsmith42@tutanota.com>
2022-02-19 11:11:03 +0000

    Test CI implementation

--- EssentialFeed/EssentialFeedTests/EssentialFeedTests.swift
+++ EssentialFeed/EssentialFeedTests/EssentialFeedTests.swift
@@ -11,7 +11,7 @@ import XCTest
 class EssentialFeedTests: XCTestCase {
 
     func test_zero() {
-//        XCTFail("Tests not yet implemented")
+        XCTFail("Tests not yet implemented")
     }
 
 }
