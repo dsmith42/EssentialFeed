bdc150876bbe5db678f19d1257ec58b69137687f
Dan Smith <dsmith42@tutanota.com>
2022-02-19 12:42:41 +0000

    Move RemoteFeedLoader components into production

---
+++ EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Dan Smith on 19/02/2022.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public final class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+
+    public func load() {
+        client.get(from: url)
+    }
+}
