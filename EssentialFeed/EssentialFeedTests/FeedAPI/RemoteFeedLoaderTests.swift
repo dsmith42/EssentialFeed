253fb1b2a8c85c5e275cad4bb435d2426857c73e
Dan Smith <dsmith42@tutanota.com>
2022-02-24 07:52:14 +0000

    Adds extension on XCTestCase to share MemoryLeakTracking between test classes.

--- EssentialFeed/EssentialFeedTests/FeedAPI/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedAPI/RemoteFeedLoaderTests.swift
@@ -123,12 +123,6 @@ class RemoteFeedLoaderTests: XCTestCase {
         return .failure(error)
     }
 
-    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.")
-        }
-    }
-
     private func makeItem(id: UUID = UUID(),
                           description: String? = nil,
                           location: String? = nil,
