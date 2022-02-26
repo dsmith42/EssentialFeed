22c703c6effc9c5586b4f6ba23445327ef7a5c96
Dan Smith <dsmith42@tutanota.com>
2022-02-26 11:27:33 +0000

    Fix teardown function override (remove `class`)

--- EssentialFeed/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -34,7 +34,7 @@ class URLSessionHTTPClientTests: XCTestCase {
         URLProtocolStub.startInterceptingRequests()
     }
 
-    override class func tearDown() {
+    override func tearDown() {
         URLProtocolStub.stopInterceptingRequests()
     }
 
