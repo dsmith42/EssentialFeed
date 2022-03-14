9f77c58b786f37120ed05e49bf75e303ced5ad0b
Dan Smith <dsmith42@tutanota.com>
2022-03-14 11:33:59 +0000

    Add line and file to memory leak tracking of makeSUT

--- EssentialFeed/EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -39,11 +39,11 @@ final class CacheFeedUseCaseTests: XCTestCase {
     }
 
     // MARK: - Helpers -
-    func makeSUT() -> (sut: LocalFeedLoader, store: FeedStore) {
+    func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
         let store = FeedStore()
         let sut = LocalFeedLoader(store: store)
-        trackForMemoryLeaks(store)
-        trackForMemoryLeaks(sut)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
         return (sut: sut, store: store)
     }
 
