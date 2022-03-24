946bf69a8456e4c87508087a36298a95daeaa3a2
Dan Smith <dsmith42@tutanota.com>
2022-03-24 17:08:11 +0000

    Indentation.

--- EssentialFeed/EssentialFeedTests/FeedCache/Helpers/FeedCacheTestHelpers.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/Helpers/FeedCacheTestHelpers.swift
@@ -16,7 +16,7 @@ func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
     let models = [uniqueImage(), uniqueImage()]
     let local = models.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
     return (models: models, local: local)
-    }
+}
 
 extension Date {
     private func adding(days: Int) -> Date {
