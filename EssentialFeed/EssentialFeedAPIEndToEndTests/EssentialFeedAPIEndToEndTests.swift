859badd951bd1efcd697dc2a3b55f6ac1907462f
Dan Smith <dsmith42@tutanota.com>
2022-04-19 09:21:49 +0100

    Update API EndToEnd result to match `FeedLoader.Result`

--- EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -33,7 +33,7 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
 
     // MARK: - Helpers -
 
-    private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> LoadFeedResult? {
+    private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> FeedLoader.Result? {
         let url = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         let loader = RemoteFeedLoader(url: url, client: client)
@@ -42,7 +42,7 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
 
         let exp = expectation(description: "Wait for load completion.")
 
-        var receivedResult: LoadFeedResult?
+        var receivedResult: FeedLoader.Result?
         loader.load { result in
             receivedResult = result
             exp.fulfill()
