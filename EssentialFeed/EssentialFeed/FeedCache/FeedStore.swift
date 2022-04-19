b13a2b92344ea6dc093c2d98fe2d02d6a66c8636
Dan Smith <dsmith42@tutanota.com>
2022-04-19 12:47:54 +0100

    Spacing

--- EssentialFeed/EssentialFeed/FeedCache/FeedStore.swift
+++ EssentialFeed/EssentialFeed/FeedCache/FeedStore.swift
@@ -19,9 +19,11 @@ public protocol FeedStore {
     /// The completion handler can be invoked in any thread.
     /// Clients are responsible to dispatch on appropriate threads, if needed.
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
+
     /// The completion handler can be invoked in any thread.
     /// Clients are responsible to dispatch on appropriate threads, if needed.
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
+
     /// The completion handler can be invoked in any thread.
     /// Clients are responsible to dispatch on appropriate threads, if needed.
     func retrieve(completion: @escaping RetrievalCompletion)
