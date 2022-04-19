45f219acfc7e7b3c4f437543407330d7c30fb4bd
Dan Smith <dsmith42@tutanota.com>
2022-04-19 09:22:43 +0100

    Nest `RetrieveCachedFeedResult` into `FeedStore.RetrievalResult`

--- EssentialFeed/EssentialFeed/FeedCache/FeedStore.swift
+++ EssentialFeed/EssentialFeed/FeedCache/FeedStore.swift
@@ -7,17 +7,17 @@
 
 import Foundation
 
-public typealias RetrieveCachedFeedResult = Result<CachedFeed, Error>
-
 public enum CachedFeed {
     case empty
     case found(feed: [LocalFeedImage], timestamp: Date)
 }
 
 public protocol FeedStore {
+    typealias RetrievalResult = Result<CachedFeed, Error>
+
     typealias DeletionCompletion = (Error?) -> Void
     typealias InsertionCompletion = (Error?) -> Void
-    typealias RetrievalCompletion = (RetrieveCachedFeedResult) -> Void
+    typealias RetrievalCompletion = (RetrievalResult) -> Void
 
     /// The completion handler can be invoked in any thread.
     /// Clients are responsible to dispatch on appropriate threads, if needed.
--- EssentialFeed/EssentialFeedTests/FeedCache/FeedStoreSpecs/XCTestCase+FeedStoreSpecs.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/FeedStoreSpecs/XCTestCase+FeedStoreSpecs.swift
@@ -139,12 +139,12 @@ extension FeedStoreSpecs where Self: XCTestCase {
         return deletionError
     }
 
-    func expect(_ sut: FeedStore, toRetrieveTwice expectedResult: RetrieveCachedFeedResult, file: StaticString = #file, line: UInt = #line) {
+    func expect(_ sut: FeedStore, toRetrieveTwice expectedResult: FeedStore.RetrievalResult, file: StaticString = #file, line: UInt = #line) {
         expect(sut, toRetrieve: expectedResult, file: file, line: line)
         expect(sut, toRetrieve: expectedResult, file: file, line: line)
     }
 
-    func expect(_ sut: FeedStore, toRetrieve expectedResult: RetrieveCachedFeedResult, file: StaticString = #file, line: UInt = #line) {
+    func expect(_ sut: FeedStore, toRetrieve expectedResult: FeedStore.RetrievalResult, file: StaticString = #file, line: UInt = #line) {
         let exp = expectation(description: "Wait for cache retrieval")
 
         sut.retrieve { retrievedResult in
