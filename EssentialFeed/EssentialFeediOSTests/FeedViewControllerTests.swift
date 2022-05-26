bca8e9f2a171c15a602a5298aa8c5eedc9488363
Dan Smith <dsmith42@tutanota.com>
2022-05-26 22:11:56 +0100

    FeedImageView retry button is visible on invalid image data

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -170,6 +170,19 @@ final class FeedViewControllerTests: XCTestCase {
 		XCTAssertEqual(view1?.isShowingRetryAction, true, "Expected no retry action for second view once second image loading completes successfully")
 	}
 
+	func test_feedImageViewRetryButton_isVisibleOnInvalidImageData() {
+		let (sut, loader) = makeSUT()
+
+		sut.loadViewIfNeeded()
+		loader.completeFeedLoading(with: [makeImage()])
+
+		let view = sut.simulateFeedImageViewVisible(at: 0)
+		XCTAssertEqual(view?.isShowingRetryAction, false, "Expected no retry action while loading image")
+
+		let invalidImageData = Data("Invalid image data".utf8)
+		loader.completeImageLoading(with: invalidImageData, at: 0)
+		XCTAssertEqual(view?.isShowingRetryAction, true, "Expected retry action once image loading completes with invalid image data")
+	}
 
 	// MARK: - Helpers -
 
