547d08eaaad5e69a3635ebfe328ac1fd6a2310d7
Dan Smith <dsmith42@tutanota.com>
2022-05-27 08:41:42 +0100

    Make an MVC folder format grouping

---
+++ EssentialFeed/EssentialFeediOS/FeedImageLoader/FeedImageDataLoader.swift
@@ -0,0 +1,17 @@
+//
+//  FeedImageDataLoader.swift
+//  EssentialFeediOS
+//
+//  Created by Dan Smith on 27/05/2022.
+//
+
+import Foundation
+
+public protocol FeedImageDataLoaderTask{
+	func cancel()
+}
+
+public protocol FeedImageDataLoader {
+	typealias Result = Swift.Result<Data, Error>
+	func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+}
