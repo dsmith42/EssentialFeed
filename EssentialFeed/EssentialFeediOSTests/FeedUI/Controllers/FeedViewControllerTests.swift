426ec0348a4182a7503d83ecb385fea394a1f8f4
Dan Smith <dsmith42@tutanota.com>
2022-05-29 09:59:34 +0100

    Reuse table view cells from storyboard configuration

--- EssentialFeed/EssentialFeediOSTests/FeedUI/Controllers/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedUI/Controllers/FeedViewControllerTests.swift
@@ -239,6 +239,17 @@ final class FeedViewControllerTests: XCTestCase {
 		XCTAssertEqual(loader.cancelledImageURLs, [image0.url, image1.url], "Expected second cancelled image URL request once second image is not near visible anymore")
 	}
 
+	func test_feedImageView_doesNotRenderLoadedImageWhenNotVisibleAnymore() {
+		let (sut, loader) = makeSUT()
+		sut.loadViewIfNeeded()
+		loader.completeFeedLoading(with: [makeImage()])
+
+		let view = sut.simulateFeedImageViewNotVisible(at: 0)
+		loader.completeImageLoading(with: anyImageData())
+
+		XCTAssertNil(view?.renderedImage, "Expected no rendered image when an image load finishes after the view is not visible anymore")
+	}
+
 	// MARK: - Helpers -
 
 	private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
@@ -251,6 +262,10 @@ final class FeedViewControllerTests: XCTestCase {
 
 	private func makeImage(description: String? = nil, location: String? = nil, url: URL = anyURL()) -> FeedImage {
 		return FeedImage(id: UUID(), description: description, location: location, url: url)
+	}
+
+	private func anyImageData() -> Data {
+		UIImage.make(withColor: .red).pngData()!
 	}
 
 }
