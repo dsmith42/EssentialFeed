8531fc63d61335dea64ec159b199f1997e4089b0
Dan Smith <dsmith42@tutanota.com>
2022-06-04 21:16:46 +0100

    Move `FeedImagePresenter` and collaborators into production

--- EssentialFeed/EssentialFeedTests/FeedPresentation/FeedImagePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedPresentation/FeedImagePresenterTests.swift
@@ -8,60 +8,6 @@
 import XCTest
 import EssentialFeed
 
-protocol FeedImageView {
-	associatedtype Image
-
-	func display(_ model: FeedImageViewModel<Image>)
-}
-
-struct FeedImageViewModel<Image> {
-	let description: String?
-	let location: String?
-	let image: Image?
-	let isLoading: Bool
-	let shouldRetry: Bool
-	var hasLocation: Bool { location != nil }
-}
-
-final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
-	private var view: View
-	private let imageTransformer: (Data) -> Image?
-
-	init(view: View, imageTransformer: @escaping (Data) -> Image?) {
-		self.view = view
-		self.imageTransformer = imageTransformer
-	}
-
-	func didStartLoadingImageData(for model: FeedImage) {
-		view.display(FeedImageViewModel(
-			description: model.description,
-			location: model.location,
-			image: nil,
-			isLoading: true,
-			shouldRetry: false))
-	}
-
-	func didFinishLoadingImageData(with data: Data, for model: FeedImage) {
-		let image = imageTransformer(data)
-		view.display(FeedImageViewModel(
-			description: model.description,
-			location: model.location,
-			image: image,
-			isLoading: false,
-			shouldRetry: image == nil))
-	}
-
-	func didFinishLoadingImageData(with error: Error, for model: FeedImage) {
-		view.display(FeedImageViewModel(
-			description: model.description,
-			location: model.location,
-			image: nil,
-			isLoading: false,
-			shouldRetry: true))
-	}
-
-}
-
 final class FeedImagePresenterTests: XCTestCase {
 
 	func test_init_noImageIsLoaded() {
@@ -159,4 +105,3 @@ final class FeedImagePresenterTests: XCTestCase {
 		}
 	}
 }
-
