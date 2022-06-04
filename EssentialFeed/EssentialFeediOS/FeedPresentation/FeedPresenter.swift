2bb5b9b9e923d426103b2a336926af38d4240952
Dan Smith <dsmith42@tutanota.com>
2022-06-04 10:22:10 +0100

    Hide feed load on reload

--- EssentialFeed/EssentialFeediOS/FeedPresentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeediOS/FeedPresentation/FeedPresenter.swift
@@ -17,7 +17,7 @@ protocol FeedView {
 }
 
 struct FeedErrorViewModel {
-	let message: String
+	let message: String?
 }
 
 protocol FeedErrorView {
@@ -51,6 +51,7 @@ final class FeedPresenter {
 
 	func didStartLoadingFeed() {
 		loadingView.display(FeedLoadingViewModel(isLoading: true))
+		errorView.display(FeedErrorViewModel(message: nil))
 	}
 
 	func didFinishLoadingFeed(with feed: [FeedImage]) {
--- EssentialFeed/EssentialFeediOSTests/FeedUI/FeedUIIntegrationTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedUI/FeedUIIntegrationTests.swift
@@ -285,7 +285,7 @@ final class FeedUIIntegrationTests: XCTestCase {
 		wait(for: [exp], timeout: 1.0)
 	}
 
-	func test_loadFeedCompletion_rendersErrorMessageOnError() {
+	func test_loadFeedCompletion_rendersErrorMessageOnErrorUntilNextReload() {
 		let (sut, loader) = makeSUT()
 
 		sut.loadViewIfNeeded()
@@ -294,6 +294,9 @@ final class FeedUIIntegrationTests: XCTestCase {
 
 		loader.completeFeedLoadingWithError(at: 0)
 		XCTAssertEqual(sut.errorMessage, localized("FEED_VIEW_CONNECTION_ERROR"))
+
+		sut.simulateUserInitiatedFeedReload()
+		XCTAssertEqual(sut.errorMessage, nil)
 	}
 
 	// MARK: - Helpers -
