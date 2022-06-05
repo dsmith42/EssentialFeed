6bbe79ae0fb65e4547702dfe2ee87fda70ebb13c
Dan Smith <dsmith42@tutanota.com>
2022-06-05 11:34:27 +0100

    Fix typo

--- EssentialFeed/EssentialFeed/FeedAPI/URLSessionHTTPClient.swift
+++ EssentialFeed/EssentialFeed/FeedAPI/URLSessionHTTPClient.swift
@@ -9,21 +9,21 @@ import Foundation
 
 public final class URLSessionHTTPClient: HTTPClient {
 	private let session: URLSession
-	
+
 	public init(session: URLSession = .shared) {
 		self.session = session
 	}
-	
-	private struct UnexpectedValuesRepresetentation: Error {}
-	
+
+	private struct UnexpectedValuesRepresentation: Error {}
+
 	private struct URLSessionTaskWrapper: HTTPClientTask {
 		let wrapped: URLSessionTask
-		
+
 		func cancel() {
 			wrapped.cancel()
 		}
 	}
-	
+
 	public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask{
 		let task = session.dataTask(with: url) { data, response, error in
 			completion(Result {
@@ -32,7 +32,7 @@ public final class URLSessionHTTPClient: HTTPClient {
 				} else if let data = data, let response = response as? HTTPURLResponse {
 					return (data, response)
 				} else {
-					throw UnexpectedValuesRepresetentation()
+					throw UnexpectedValuesRepresentation()
 				}
 			})
 		}
