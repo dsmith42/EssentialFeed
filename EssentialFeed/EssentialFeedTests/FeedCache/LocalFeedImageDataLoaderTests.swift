b75ef0ec7eeb5191d908f88d8f5052a7ea04e0ed
Dan Smith <dsmith42@tutanota.com>
2022-06-05 18:28:24 +0100

    `LocalFeedImageDataLoader.loadImageData` requests stored data for URL from `FeedImageDataStore`

--- EssentialFeed/EssentialFeedTests/FeedCache/LocalFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/LocalFeedImageDataLoaderTests.swift
@@ -8,8 +8,25 @@
 import XCTest
 import EssentialFeed
 
+protocol FeedImageDataStore {
+	func retrieve(dataForURL url: URL)
+}
+
 final class LocalFeedImageDataLoader {
-	init(store: Any) {}
+	private struct Task: FeedImageDataLoaderTask {
+		func cancel() {}
+	}
+
+	private let store: FeedImageDataStore
+
+	init(store: FeedImageDataStore) {
+		self.store = store
+	}
+
+	func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+		store.retrieve(dataForURL: url)
+		return Task()
+	}
 }
 
 class LocalFeedImageDataLoaderTests: XCTestCase {
@@ -20,17 +37,34 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
 		XCTAssertTrue(store.receivedMessages.isEmpty)
 	}
 
+	func test_LoadImageDataFromURL_requestsStoredDataForURL() {
+		let (sut, store) = makeSUT()
+		let url = anyURL()
+
+		_ = sut.loadImageData(from: url) { _ in }
+
+		XCTAssertEqual(store.receivedMessages, [.retrieve(dataFor: url)])
+	}
+
 	// MARK: - Helpers
 
-	private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: FeedStoreSpy) {
-		let store = FeedStoreSpy()
+	private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: StoreSpy) {
+		let store = StoreSpy()
 		let sut = LocalFeedImageDataLoader(store: store)
 		trackForMemoryLeaks(store, file: file, line: line)
 		trackForMemoryLeaks(sut, file: file, line: line)
 		return (sut, store)
 	}
 
-	private class FeedStoreSpy {
-		let receivedMessages = [Any]()
+	private class StoreSpy: FeedImageDataStore {
+		enum Message: Equatable {
+			case retrieve(dataFor: URL)
+		}
+
+		private(set) var receivedMessages = [Message]()
+
+		func retrieve(dataForURL url: URL) {
+			receivedMessages.append(.retrieve(dataFor: url))
+		}
 	}
 }
