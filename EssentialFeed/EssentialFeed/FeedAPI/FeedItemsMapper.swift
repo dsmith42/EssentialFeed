0e309b2d230717ed398184be059113e185abe63d
Dan Smith <dsmith42@tutanota.com>
2022-06-05 13:57:55 +0100

    Extract `HTTPURLResponse.statusCode == 200` validation
    into a `isOK` helper method

--- EssentialFeed/EssentialFeed/FeedAPI/FeedItemsMapper.swift
+++ EssentialFeed/EssentialFeed/FeedAPI/FeedItemsMapper.swift
@@ -15,7 +15,7 @@ final class FeedItemsMapper {
 	private static var OK_200: Int { return 200 }
 
 	static func map(_ data: Data, from response: HTTPURLResponse) throws ->  [RemoteFeedItem] {
-		guard response.statusCode == OK_200,
+		guard response.isOK,
 			  let root = try? JSONDecoder().decode(Root.self, from: data) else {
 				  throw RemoteFeedLoader.Error.invalidData
 			  }
