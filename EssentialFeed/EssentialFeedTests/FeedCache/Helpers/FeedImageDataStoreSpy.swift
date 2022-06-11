236eaf975544d7dae7d3fa94c58bd4539e70814e
Dan Smith <dsmith42@tutanota.com>
2022-06-11 13:51:26 +0100

    Extract `FeedImageDataStoreSpy` into a shared scope

---
+++ EssentialFeed/EssentialFeedTests/FeedCache/Helpers/FeedImageDataStoreSpy.swift
@@ -0,0 +1,36 @@
+//
+//  FeedImageDataStoreSpy.swift
+//  EssentialFeedTests
+//
+//  Created by Dan Smith on 11/06/2022.
+//
+
+import Foundation
+import EssentialFeed
+
+class StoreSpy: FeedImageDataStore {
+	enum Message: Equatable {
+		case retrieve(dataFor: URL)
+		case insert(data: Data, for: URL)
+	}
+
+	private var retrievalCompletions = [(FeedImageDataStore.RetrievalResult) -> Void]()
+	private(set) var receivedMessages = [Message]()
+
+	func insert(_ data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void) {
+		receivedMessages.append(.insert(data: data, for: url))
+	}
+
+	func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+		receivedMessages.append(.retrieve(dataFor: url))
+		retrievalCompletions.append(completion)
+	}
+
+	func completeRetrieval(with error: Error, at index: Int = 0) {
+		retrievalCompletions[index](.failure(error))
+	}
+
+	func completeRetrieval(with data: Data?, at index: Int = 0) {
+		retrievalCompletions[index](.success(data))
+	}
+}
--- EssentialFeed/EssentialFeedTests/FeedCache/LoadFeedImageDataFromUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/LoadFeedImageDataFromUseCaseTests.swift
@@ -133,33 +133,6 @@ class LoadFeedImageDataFromUseCaseTests: XCTestCase {
 		action()
 
 		wait(for: [exp], timeout: 1.0)
-
 	}
 
-	private class StoreSpy: FeedImageDataStore {
-		enum Message: Equatable {
-			case retrieve(dataFor: URL)
-			case insert(data: Data, for: URL)
-		}
-
-		private var retrievalCompletions = [(FeedImageDataStore.RetrievalResult) -> Void]()
-		private(set) var receivedMessages = [Message]()
-
-		func insert(_ data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void) {
-			receivedMessages.append(.insert(data: data, for: url))
-		}
-
-		func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
-			receivedMessages.append(.retrieve(dataFor: url))
-			retrievalCompletions.append(completion)
-		}
-
-		func completeRetrieval(with error: Error, at index: Int = 0) {
-			retrievalCompletions[index](.failure(error))
-		}
-
-		func completeRetrieval(with data: Data?, at index: Int = 0) {
-			retrievalCompletions[index](.success(data))
-		}
-	}
 }
