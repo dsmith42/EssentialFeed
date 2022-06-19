e5e59974710ef39318e8eed2ea5505c7c2628329
Dan Smith <dsmith42@tutanota.com>
2022-06-19 20:17:10 +0100

    Separate test scopes with better naming

--- EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -17,34 +17,36 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
 	override func tearDown() {
 		undoStoreSideEffects()
 	}
-	
+
+	// MARK: - LocalFeedLoader Tests
+
 	func test_load_deliversNoItemsOnEmptyCache() {
-		let sut = makeFeedLoader()
+		let feedLoader = makeFeedLoader()
 		
-		expect(sut, toLoad: [])
+		expect(feedLoader, toLoad: [])
 	}
 	
 	func test_load_deliversItemsSavedOnASeparateInstance() throws {
-		let sutToPerformSave = makeFeedLoader()
-		let sutToPerformLoad = makeFeedLoader()
+		let feedLoaderToPerformSave = makeFeedLoader()
+		let feedLoaderToPerformLoad = makeFeedLoader()
 		let feed = uniqueImageFeed().models
 		
-		save(feed, with: sutToPerformSave)
+		save(feed, with: feedLoaderToPerformSave)
 		
-		expect(sutToPerformLoad, toLoad: feed)
+		expect(feedLoaderToPerformLoad, toLoad: feed)
 	}
 	
 	func test_save_overridesItemsSavedOnASeparateInstance() {
-		let sutToPerformFirstSave = makeFeedLoader()
-		let sutToPerformLastSave = makeFeedLoader()
-		let sutToPerformLoad = makeFeedLoader()
+		let feedLoaderToPerformFirstSave = makeFeedLoader()
+		let feedLoaderToPerformLastSave = makeFeedLoader()
+		let feedLoaderToPerformLoad = makeFeedLoader()
 		let firstFeed = uniqueImageFeed().models
 		let latestFeed = uniqueImageFeed().models
 		
-		save(firstFeed, with: sutToPerformFirstSave)
-		save(latestFeed, with: sutToPerformLastSave)
+		save(firstFeed, with: feedLoaderToPerformFirstSave)
+		save(latestFeed, with: feedLoaderToPerformLastSave)
 		
-		expect(sutToPerformLoad, toLoad: latestFeed)
+		expect(feedLoaderToPerformLoad, toLoad: latestFeed)
 	}
 
 	// MARK: _ LocalFeedImageDataLoader Tests
