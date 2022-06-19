597546689d874dd47fb29f12406dbd2170a43328
Dan Smith <dsmith42@tutanota.com>
2022-06-19 20:22:55 +0100

    LocalFeedImageDataLoader in integration with the `CoreDataFeedStore` overrides items saved by separate instances, proving we correctly manage the data models on disk.

--- EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -64,6 +64,22 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
 		expect(imageLoaderToPerformLoad, toLoad: dataToSave, for: image.url)
 	}
 
+	func test_saveImageData_overridesSavedImageDataOnASeparateInstance() {
+		let imageLoaderToPerformFirstSave = makeImageLoader()
+		let imageLoaderToPerformLastSave = makeImageLoader()
+		let imageLoaderToPerformLoad = makeImageLoader()
+		let feedLoader = makeFeedLoader()
+		let image = uniqueImage()
+		let firstImageData = Data("first".utf8)
+		let lastImageData = Data("last".utf8)
+
+		save([image], with: feedLoader)
+		save(firstImageData, for: image.url, with: imageLoaderToPerformFirstSave)
+		save(lastImageData, for: image.url, with: imageLoaderToPerformLastSave)
+
+		expect(imageLoaderToPerformLoad, toLoad: lastImageData, for: image.url)
+	}
+
 	// MARK: - Helpers -
 	
 	private func makeFeedLoader(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
@@ -122,7 +138,7 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
 		wait(for: [saveExp], timeout: 1.0)
 	}
 
-	private func expect(_ sut: LocalFeedImageDataLoader, toLoade expectedData: Data, for url: URL, file: StaticString = #file, line: UInt = #line) {
+	private func expect(_ sut: LocalFeedImageDataLoader, toLoad expectedData: Data, for url: URL, file: StaticString = #file, line: UInt = #line) {
 		let exp = expectation(description: "Wait for load completion")
 		_ = sut.loadImageData(from: url) { result in
 			switch result {
