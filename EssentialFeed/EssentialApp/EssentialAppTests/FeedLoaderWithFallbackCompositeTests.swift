25e16dafe6299d2a1d9ca0aff3f68b5c90677fd7
Dan Smith <dsmith42@tutanota.com>
2022-07-12 06:41:18 +0100

    `FeedLoaderWithFallbackComposite` delivers fallback feed on primary loader failure

--- EssentialFeed/EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ EssentialFeed/EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -11,13 +11,23 @@ import EssentialFeed
 
 class FeedLoaderWithFallbackComposite: FeedLoader {
 	private let primary: FeedLoader
+	private let fallback: FeedLoader
 
 	init(primary: FeedLoader, fallback: FeedLoader) {
 		self.primary = primary
+		self.fallback = fallback
 	}
 
 	func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		primary.load(completion: completion)
+		primary.load { [weak self] result in
+			switch result {
+			case .success:
+				completion(result)
+
+			case .failure:
+				self?.fallback.load(completion: completion)
+			}
+		}
 	}
 }
 
@@ -35,6 +45,27 @@ class FeedLoaderWithFallbackCompositeTests: XCTestCase {
 			switch result {
 			case let .success(receievedFeed):
 				XCTAssertEqual(receievedFeed, primaryFeed)
+
+			case .failure:
+				XCTFail("Expected successful load feed result, got \(result) instead")
+			}
+
+			exp.fulfill()
+		}
+		wait(for: [exp], timeout: 1.0)
+	}
+
+	func test_load_deliversFallbackFeedOnPrimaryFailure() {
+		let fallbackFeed = uniqueFeed()
+
+		let sut = makeSUT(primaryResult: .failure(anyNSError()), fallbackResult: .success(fallbackFeed))
+
+		let exp = expectation(description: "Wait for load completion")
+
+		sut.load { result in
+			switch result {
+			case let .success(receievedFeed):
+				XCTAssertEqual(receievedFeed, fallbackFeed)
 
 			case .failure:
 				XCTFail("Expected successful load feed result, got \(result) instead")
@@ -65,6 +96,10 @@ class FeedLoaderWithFallbackCompositeTests: XCTestCase {
 
 	private func uniqueFeed() -> [FeedImage] {
 		return [FeedImage(id: UUID(), description: "any", location: "any", url: URL(string: "http://any-url.com")!)]
+	}
+
+	private func anyNSError() -> NSError {
+		return NSError(domain: "any error", code: 0)
 	}
 
 	private class LoaderStub: FeedLoader {
