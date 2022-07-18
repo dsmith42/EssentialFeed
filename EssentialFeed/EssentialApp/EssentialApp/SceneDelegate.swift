a72dba81c8e109bec7f8fc2156d6e37ba777788b
Dan Smith <dsmith42@tutanota.com>
2022-07-18 06:35:41 +0100

    Compose feedLoaders and update CI configuration

--- EssentialFeed/EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialFeed/EssentialApp/EssentialApp/SceneDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 import EssentialFeed
 import EssentialFeediOS
+import CoreData
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	var window: UIWindow?
@@ -15,16 +16,25 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 		guard let _ = (scene as? UIWindowScene) else { return }
 
-		let url = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+		let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+
 		let session = URLSession(configuration: .ephemeral)
-		let client = URLSessionHTTPClient(session: session)
-		let feedLoader = RemoteFeedLoader(url: url, client: client)
-		let imageLoader = RemoteFeedImageDataLoader(client: client)
+		let remoteClient = URLSessionHTTPClient(session: session)
+		let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: remoteClient)
+		let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
 
-		let feedViewController = FeedUIComposer.feedComposedWith(feedLoader: feedLoader,
-																														 imageLoader: imageLoader)
+		let localStoreURL = NSPersistentContainer
+			.defaultDirectoryURL()
+			.appendingPathComponent("feed-store.sqlite")
 
-		window?.rootViewController = feedViewController
+		let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
+		let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
+		let localImageLoader = LocalFeedImageDataLoader(store: localStore)
+
+		let feedloaderComposite = FeedLoaderWithFallbackComposite(primary: remoteFeedLoader, fallback: localFeedLoader)
+		let feedImageLoaderComposite = FeedImageDataLoaderWithFallbackComposite(primary: localImageLoader, fallback: remoteImageLoader)
+
+		window?.rootViewController = FeedUIComposer.feedComposedWith(feedLoader: feedloaderComposite, imageLoader: feedImageLoaderComposite)
 	}
 
 }
