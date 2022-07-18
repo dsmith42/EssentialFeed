18c1453056623ad3d8896e759b82ce341c3a5ba1
Dan Smith <dsmith42@tutanota.com>
2022-07-18 17:56:48 +0100

    Move `FeedCache` into the `FeedFeature` production group

--- EssentialFeed/EssentialFeed/FeedFeature/FeedCache.swift
+++ EssentialFeed/EssentialFeed/FeedFeature/FeedCache.swift
@@ -5,4 +5,9 @@
 //  Created by Dan Smith on 18/07/2022.
 //
 
-import Foundation
+public protocol FeedCache {
+	typealias Result = Swift.Result<Void, Error>
+
+	func save(_ feed: [FeedImage], completion: @escaping (Result) -> Void)
+}
+
