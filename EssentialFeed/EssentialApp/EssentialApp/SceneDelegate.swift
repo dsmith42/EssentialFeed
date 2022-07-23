9b6d3004f6ef2cf8aea59758cc1750da08dfcb2a
Dan Smith <dsmith42@tutanota.com>
2022-07-23 10:19:49 +0100

    Add UI Test target for running high level acceptance tests

--- EssentialFeed/EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialFeed/EssentialApp/EssentialApp/SceneDelegate.swift
@@ -8,36 +8,10 @@
 import UIKit
 import EssentialFeed
 import EssentialFeediOS
-import CoreData
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	var window: UIWindow?
 
-	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-		guard let _ = (scene as? UIWindowScene) else { return }
-
-		let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
-
-		let session = URLSession(configuration: .ephemeral)
-		let remoteClient = URLSessionHTTPClient(session: session)
-		let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: remoteClient)
-		let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
-
-		let localStoreURL = NSPersistentContainer
-			.defaultDirectoryURL()
-			.appendingPathComponent("feed-store.sqlite")
-
-		let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
-		let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
-		let localImageLoader = LocalFeedImageDataLoader(store: localStore)
-
-		let primaryFeedLoader = FeedLoaderCacheDecorator(decoratee: remoteFeedLoader, cache: localFeedLoader)
-
-		let feedloaderComposite = FeedLoaderWithFallbackComposite(primary: primaryFeedLoader, fallback: localFeedLoader)
-		let feedImageLoaderComposite = FeedImageDataLoaderWithFallbackComposite(primary: localImageLoader, fallback: remoteImageLoader)
-
-		window?.rootViewController = FeedUIComposer.feedComposedWith(feedLoader: feedloaderComposite, imageLoader: feedImageLoaderComposite)
-	}
 
 }
 
