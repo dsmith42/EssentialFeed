f7cd8341b1fdf77e73c822b422091ad9ecf53365
Dan Smith <dsmith42@tutanota.com>
2022-07-28 17:43:39 +0100

    Remove unnecessary `Main.storyboard` the initial setup happens in code

--- EssentialFeed/EssentialApp/EssentialAppTests/SceneDelegateTests.swift
+++ EssentialFeed/EssentialApp/EssentialAppTests/SceneDelegateTests.swift
@@ -11,17 +11,37 @@ import EssentialFeediOS
 
 final class SceneDelegateTests: XCTestCase {
 
-    func test_sceneWillConnectTo_configureRootViewController() {
-			let sut = SceneDelegate()
-			sut.window = UIWindow()
+	func test_configureWindow_setsWindowAsKeyAndVisible() {
+		let window = UIWindowSpy()
+		let sut = SceneDelegate()
+		sut.window = window
 
-			sut.configureWindow()
+		sut.configureWindow()
 
-			let root = sut.window?.rootViewController
-			let rootNavigation = root as? UINavigationController
-			let topController = rootNavigation?.topViewController
+		XCTAssertEqual(window.makeKeyAndVisibleCallCount, 1, "Expected to make window key and visible")
+	}
 
-			XCTAssertNotNil(rootNavigation, "Expected a navigation controller as root, got \(String(describing: root)) instead")
-			XCTAssertTrue(topController is FeedViewController, "Expected a feed controller as top controller, got \(String(describing: topController)) instead")
+	func test_sceneWillConnectTo_configureRootViewController() {
+		let sut = SceneDelegate()
+		sut.window = UIWindow()
+
+		sut.configureWindow()
+
+		let root = sut.window?.rootViewController
+		let rootNavigation = root as? UINavigationController
+		let topController = rootNavigation?.topViewController
+
+		XCTAssertNotNil(rootNavigation, "Expected a navigation controller as root, got \(String(describing: root)) instead")
+		XCTAssertTrue(topController is FeedViewController, "Expected a feed controller as top controller, got \(String(describing: topController)) instead")
+	}
+
+	// MARK: - Helpers
+
+	private class UIWindowSpy: UIWindow {
+		var makeKeyAndVisibleCallCount = 0
+
+		override func makeKeyAndVisible() {
+			makeKeyAndVisibleCallCount += 1
 		}
+	}
 }
