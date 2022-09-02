ab6b038ddc59e9f095a02cf1d854ca7c7c862819
Dan Smith <dsmith42@tutanota.com>
2022-09-02 15:12:38 +0100

    Replace FeedLoader composition with Combine operations.

--- EssentialFeed/EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialFeed/EssentialApp/EssentialApp/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Combine
 import CoreData
 import EssentialFeed
 import EssentialFeediOS
@@ -43,28 +44,101 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	}
 
 	func configureWindow() {
-		let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
-
-		let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: httpClient)
 		let remoteImageLoader = RemoteFeedImageDataLoader(client: httpClient)
-
-		let localFeedLoader = LocalFeedLoader(store: feedStore, currentDate: Date.init)
 		let localImageLoader = LocalFeedImageDataLoader(store: feedStore)
 
 		window?.rootViewController = UINavigationController(
 			rootViewController: FeedUIComposer.feedComposedWith(
-				feedLoader: FeedLoaderWithFallbackComposite(
-					primary: FeedLoaderCacheDecorator(decoratee: remoteFeedLoader,
-																						cache: localFeedLoader),
-					fallback: localFeedLoader),
+				feedLoader: makeRemoteFeedLoaderWithLocalFallback,
 				imageLoader: FeedImageDataLoaderWithFallbackComposite(
 					primary: localImageLoader,
-					fallback: FeedImageDataLoaderDecorator(decoratee: remoteImageLoader,
-																								 cache: localImageLoader))))
-		window?.makeKeyAndVisible()
+					fallback: FeedImageDataLoaderDecorator(
+						decoratee: remoteImageLoader,
+						cache: localImageLoader))))
+			window?.makeKeyAndVisible()
 	}
 
 	func sceneWillResignActive(_ scene: UIScene) {
 		localFeedLoader.validateCache { _ in }
 	}
+
+	func makeRemoteFeedLoaderWithLocalFallback() -> FeedLoader.Publisher {
+		let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+		let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: httpClient)
+		return remoteFeedLoader
+			.loadPublisher()
+			.caching(to: localFeedLoader)
+			.fallback(to: localFeedLoader.loadPublisher)
+	}
 }
+
+public extension FeedLoader {
+	typealias Publisher = AnyPublisher<[FeedImage], Error>
+
+	func loadPublisher() -> Publisher {
+		Deferred {
+			Future(self.load)
+		}
+		.eraseToAnyPublisher()
+	}
+}
+
+extension Publisher where Output == [FeedImage] {
+	func caching(to cache: FeedCache) -> AnyPublisher<Output, Failure> {
+		handleEvents(receiveOutput: cache.saveIgnoringResult).eraseToAnyPublisher()
+	}
+}
+
+private extension FeedCache {
+	func saveIgnoringResult(_ feed: [FeedImage]) {
+		save(feed) { _ in }
+	}
+}
+
+extension Publisher {
+	func fallback(to fallBackPublisher: @escaping () -> AnyPublisher<Output, Failure>) -> AnyPublisher<Output, Failure> {
+		self.catch { _ in fallBackPublisher() }.eraseToAnyPublisher()
+	}
+}
+
+extension Publisher {
+	func dispatchOnMainQueue() -> AnyPublisher<Output, Failure> {
+		receive(on: DispatchQueue.immediateWhenOnMainQueueScheduler).eraseToAnyPublisher()
+	}
+}
+
+extension DispatchQueue {
+	static var immediateWhenOnMainQueueScheduler: ImmediateWhenOnMainQueueScheduler {
+		ImmediateWhenOnMainQueueScheduler()
+	}
+
+	struct ImmediateWhenOnMainQueueScheduler: Scheduler {
+		typealias SchedulerTimeType = DispatchQueue.SchedulerTimeType
+		typealias SchedulerOptions = DispatchQueue.SchedulerOptions
+
+		var now: SchedulerTimeType {
+			DispatchQueue.main.now
+		}
+
+		var minimumTolerance: SchedulerTimeType.Stride {
+			DispatchQueue.main.minimumTolerance
+		}
+
+		func schedule(options: SchedulerOptions?, _ action: @escaping () -> Void) {
+			guard Thread.isMainThread else {
+				return DispatchQueue.main.schedule(options: options, action)
+			}
+
+			action()
+		}
+
+		func schedule(after date: DispatchQueue.SchedulerTimeType, tolerance: DispatchQueue.SchedulerTimeType.Stride, options: DispatchQueue.SchedulerOptions?, _ action: @escaping () -> Void) {
+			DispatchQueue.main.schedule(after: date, tolerance: tolerance, options: options, action)
+		}
+
+		func schedule(after date: DispatchQueue.SchedulerTimeType, interval: DispatchQueue.SchedulerTimeType.Stride, tolerance: DispatchQueue.SchedulerTimeType.Stride, options: DispatchQueue.SchedulerOptions?, _ action: @escaping () -> Void) -> Cancellable {
+			DispatchQueue.main.schedule(after: date, interval: interval, tolerance: tolerance, options: options, action)
+		}
+	}
+}
+
--- EssentialFeed/EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
+++ EssentialFeed/EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
@@ -318,7 +318,7 @@ public class FeedUIIntegrationTests: XCTestCase {
 
 	private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
 		let loader = LoaderSpy()
-		let sut = FeedUIComposer.feedComposedWith(feedLoader: loader, imageLoader: loader)
+		let sut = FeedUIComposer.feedComposedWith(feedLoader: loader.loadPublisher, imageLoader: loader)
 		trackForMemoryLeaks(loader, file: file, line: line)
 		trackForMemoryLeaks(sut, file: file, line: line)
 		return (sut, loader)
