3119161de82723c3684785bcdf09544161ca54cd
Dan Smith <dsmith42@tutanota.com>
2022-09-02 12:41:19 +0100

    Indentation

--- EssentialFeed/EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialFeed/EssentialApp/EssentialApp/SceneDelegate.swift
@@ -12,45 +12,45 @@ import EssentialFeediOS
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	var window: UIWindow?
-	
+
 	private lazy var httpClient: HTTPClient = {
 		URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
 	}()
-	
+
 	private lazy var feedStore: FeedStore & FeedImageDataStore = {
 		try! CoreDataFeedStore.init(
 			storeURL: NSPersistentContainer
 				.defaultDirectoryURL()
 				.appendingPathComponent("feed-store.sqlite"))
 	}()
-	
+
 	private lazy var localFeedLoader: LocalFeedLoader = {
 		LocalFeedLoader(store: feedStore, currentDate: Date.init)
 	}()
-	
+
 	convenience init(httpClient: HTTPClient, store: FeedStore & FeedImageDataStore) {
 		self.init()
 		self.httpClient = httpClient
 		self.feedStore = store
 	}
-	
+
 	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 		guard let scene = (scene as? UIWindowScene) else { return }
-		
+
 		window = UIWindow(windowScene: scene)
-		
+
 		configureWindow()
 	}
-	
+
 	func configureWindow() {
 		let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
-		
+
 		let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: httpClient)
 		let remoteImageLoader = RemoteFeedImageDataLoader(client: httpClient)
-		
+
 		let localFeedLoader = LocalFeedLoader(store: feedStore, currentDate: Date.init)
 		let localImageLoader = LocalFeedImageDataLoader(store: feedStore)
-		
+
 		window?.rootViewController = UINavigationController(
 			rootViewController: FeedUIComposer.feedComposedWith(
 				feedLoader: FeedLoaderWithFallbackComposite(
@@ -63,7 +63,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 																								 cache: localImageLoader))))
 		window?.makeKeyAndVisible()
 	}
-	
+
 	func sceneWillResignActive(_ scene: UIScene) {
 		localFeedLoader.validateCache { _ in }
 	}
