914ad6da33fa5c729d66bec299183417bb0d9ef5
Dan Smith <73941747+dsmith42@users.noreply.github.com>
2022-09-03 16:31:36 +0100

    Test FeedImageDataMapper in isolation

---
+++ EssentialFeed/EssentialFeed/FeedAPI/FeedImageDataMapper.swift
@@ -0,0 +1,21 @@
+//
+//  FeedImageDataMapper.swift
+//  EssentialFeed
+//
+//  Created by Dan Smith on 03/09/2022.
+//
+
+import Foundation
+
+public final class FeedImageDataMapper {
+	public enum Error: Swift.Error {
+		case invalidData
+	}
+
+	public static func map(_ data: Data, from response: HTTPURLResponse) throws -> Data {
+		guard response.isOK, !data.isEmpty else {
+			throw Error.invalidData
+		}
+		return data
+	}
+}
