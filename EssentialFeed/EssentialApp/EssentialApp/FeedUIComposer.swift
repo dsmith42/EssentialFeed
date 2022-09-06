f72d1a9de5ce495622ed651d8ae362394094b8f7
Dan Smith <73941747+dsmith42@users.noreply.github.com>
2022-09-06 11:51:52 +0100

    Add typealias to tidy up FeedUIComposer a little

--- EssentialFeed/EssentialApp/EssentialApp/FeedUIComposer.swift
+++ EssentialFeed/EssentialApp/EssentialApp/FeedUIComposer.swift
@@ -13,8 +13,9 @@ import EssentialFeediOS
 public final class FeedUIComposer {
 	private init() {}
 
+	private typealias FeedPresentationAdapter = LoadResourcePresentationAdapter<[FeedImage], FeedViewAdapter>
 	public static func feedComposedWith(feedLoader: @escaping () -> AnyPublisher<[FeedImage], Error>, imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher) -> FeedViewController {
-		let presentationAdapter = LoadResourcePresentationAdapter<[FeedImage], FeedViewAdapter>(loader: { feedLoader().dispatchOnMainQueue() })
+		let presentationAdapter = FeedPresentationAdapter( loader: { feedLoader().dispatchOnMainQueue() } )
 
 		let feedController = makeFeedViewController(
 			delegate: presentationAdapter,
