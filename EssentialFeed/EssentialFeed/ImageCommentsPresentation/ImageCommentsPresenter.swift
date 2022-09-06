0bc9d1633dcb777cee7b3f0e5048c5619bd537ba
Dan Smith <73941747+dsmith42@users.noreply.github.com>
2022-09-06 11:23:41 +0100

    Inject current date, locale and calendar to make tests reliable any locale

--- EssentialFeed/EssentialFeed/ImageCommentsPresentation/ImageCommentsPresenter.swift
+++ EssentialFeed/EssentialFeed/ImageCommentsPresentation/ImageCommentsPresenter.swift
@@ -32,13 +32,20 @@ public final class ImageCommentsPresenter {
 											comment: "Title for the image comments view")
 	}
 
-	public static func map(_ comments: [ImageComment]) -> ImageCommentsViewModel {
+	public static func map(
+		_ comments: [ImageComment],
+		currentDate: Date = Date(),
+		calendar: Calendar = .current,
+		locale: Locale = .current
+	) -> ImageCommentsViewModel {
 		let formatter = RelativeDateTimeFormatter()
+		formatter.locale = locale
+		formatter.calendar = calendar
 
 		return ImageCommentsViewModel(comments: comments.map { comment in
 			ImageCommentViewModel(
 				message: comment.message,
-				date: formatter.localizedString(for: comment.createdAt, relativeTo: Date()),
+				date: formatter.localizedString(for: comment.createdAt, relativeTo: currentDate),
 				username: comment.username)
 		})
 	}
--- EssentialFeed/EssentialFeedTests/ImageCommentsPresentation/ImageCommentsPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/ImageCommentsPresentation/ImageCommentsPresenterTests.swift
@@ -16,7 +16,8 @@ final class ImageCommentsPresenterTests: XCTestCase {
 
 	func test_map_createsViewModel() {
 		let now = Date()
-
+		let calendar = Calendar(identifier: .gregorian)
+		let locale = Locale(identifier: "en_US_POSIX")
 		let comments = [
 			ImageComment(id: UUID(),
 									 message: "a message",
@@ -28,7 +29,12 @@ final class ImageCommentsPresenterTests: XCTestCase {
 									 username: "another username"),
 		]
 
-		let viewModel = ImageCommentsPresenter.map(comments)
+		let viewModel = ImageCommentsPresenter.map(
+			comments,
+			currentDate: now,
+			calendar: calendar,
+			locale: locale
+		)
 
 		XCTAssertEqual(viewModel.comments, [
 			ImageCommentViewModel(
