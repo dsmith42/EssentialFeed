9cac79661214eefe1c9895d943823917550ddd31
Dan Smith <73941747+dsmith42@users.noreply.github.com>
2022-09-08 13:12:57 +0100

    Replace hard coded fonts with dynamic fonts

--- EssentialFeed/EssentialFeediOSTests/ImageCommentsUI/ImageCommentsSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/ImageCommentsUI/ImageCommentsSnapshotTests.swift
@@ -18,6 +18,7 @@ final class ImageCommentsSnapshotTests: XCTestCase {
 
 			assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "IMAGE_COMMENTS_light")
 			assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "IMAGE_COMMENTS_dark")
+			assert(snapshot: sut.snapshot(for: .iPhone8(style: .light, contentSize: .extraExtraExtraLarge)), named: "IMAGE_COMMENTS_extraExtraExtraLarge")
     }
 
 	// MARK: - Helpers
