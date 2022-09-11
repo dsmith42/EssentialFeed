1eab3675cfa70d8b8dda39c682947bd939a655c1
Dan Smith <73941747+dsmith42@users.noreply.github.com>
2022-09-11 10:53:59 +0100

    Add "Load more" cell controller with activity spinner

---
+++ EssentialFeed/EssentialFeediOS/FeedUI/Controllers/LoadMoreCellController.swift
@@ -0,0 +1,27 @@
+//
+//  LoadMoreCellController.swift
+//  EssentialFeediOS
+//
+//  Created by Dan Smith on 10/09/2022.
+//
+
+import Foundation
+import UIKit
+import EssentialFeed
+
+public final class LoadMoreCellController: NSObject, UITableViewDataSource {
+	private let cell = LoadMoreCell()
+
+	public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		1
+	}
+
+	public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		cell
+	}
+}
+
+extension LoadMoreCellController: ResourceLoadingView {
+	public func display(_ viewModel: ResourceLoadingViewModel) {
+		cell.isLoading = viewModel.isLoading	}
+}
--- EssentialFeed/EssentialFeediOSTests/FeedUI/FeedSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedUI/FeedSnapshotTests.swift
@@ -31,6 +31,16 @@ class FeedSnapshotTests: XCTestCase {
 		assert(snapshot: sut.snapshot(for: .iPhone8(style: .light, contentSize: .extraExtraExtraLarge)), named: "FEED_WITH_FAILED_IMAGE_LOADING_extraExtraExtraLarge")
 	}
 
+	func test_feedWithLoadMoreIndicator() {
+		let sut = makeSUT()
+
+		sut.display(feedWithLoadMoreIndicator())
+
+		assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_LOAD_MORE_INDICATOR_light")
+		assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_LOAD_MORE_INDICATOR_dark")
+		assert(snapshot: sut.snapshot(for: .iPhone8(style: .light, contentSize: .extraExtraExtraLarge)), named: "FEED_WITH_LOAD_MORE_INDICATOR_extraExtraExtraLarge")
+	}
+
 	// MARK: - Helpers
 
 	private func makeSUT() -> ListViewController {
@@ -66,9 +76,20 @@ class FeedSnapshotTests: XCTestCase {
 			ImageStub(description: nil,
 								location: "Brighton Seafront",
 								image: nil)
-			]
+		]
 	}
 
+	private func feedWithLoadMoreIndicator() -> [CellController] {
+		let stub = feedWithContent().last!
+		let cellController = FeedImageCellController(viewModel: stub.viewModel, delegate: stub, selection: {})
+		stub.controller = cellController
+
+		let loadMore = LoadMoreCellController()
+		loadMore.display(ResourceLoadingViewModel(isLoading: true))
+		return [CellController(id: UUID(), cellController),
+						CellController(id: UUID(), loadMore)
+		]
+	}
 }
 
 private extension ListViewController {
@@ -89,7 +110,7 @@ private class ImageStub: FeedImageCellControllerDelegate {
 	weak var controller: FeedImageCellController?
 
 	init(description: String?, location: String?, image: UIImage?) {
-		self.viewModel = FeedImageViewModel(
+		viewModel = FeedImageViewModel(
 			description: description,
 			location: location
 		)
@@ -108,5 +129,4 @@ private class ImageStub: FeedImageCellControllerDelegate {
 	}
 
 	func didCancelImageRequest() {}
-
 }
