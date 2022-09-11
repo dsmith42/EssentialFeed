a09abcf126a7c909f27a71dfe3837ae4cf660a5c
Dan Smith <73941747+dsmith42@users.noreply.github.com>
2022-09-11 18:14:39 +0100

    Add feed endpoint with after id param

--- EssentialFeed/EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialFeed/EssentialApp/EssentialApp/SceneDelegate.swift
@@ -61,7 +61,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	}
 
 	private func makeRemoteFeedLoaderWithLocalFallback() -> AnyPublisher<Paginated<FeedImage>, Error> {
-		let url = FeedEndpoint.get.url(baseURL: baseURL)
+		let url = FeedEndpoint.get().url(baseURL: baseURL)
 		return httpClient.getPublisher(url: url)
 			.tryMap(FeedItemsMapper.map)
 			.caching(to: localFeedLoader)
