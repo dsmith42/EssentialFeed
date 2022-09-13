caf944af173942e2fd73f5db679c5c70049154aa
Dan Smith <73941747+dsmith42@users.noreply.github.com>
2022-09-13 13:40:57 +0100

    Optimize CoreData implementation to reduce cache misses
    Key point is to optimize in the infrastructure layer to keep the Local Loaders simple and clean

--- EssentialFeed/EssentialFeed/FeedCache/Infrastructure/CoreData/ManagedImage.swift
+++ EssentialFeed/EssentialFeed/FeedCache/Infrastructure/CoreData/ManagedImage.swift
@@ -22,6 +22,12 @@ extension ManagedFeedImage {
 		return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
 	}
 
+	static func data(with url: URL, in context: NSManagedObjectContext) throws -> Data? {
+		if let data = context.userInfo[url] as? Data { return data }
+
+		return try first(with: url, in: context)?.data
+	}
+
 	static func first(with url: URL, in context: NSManagedObjectContext) throws -> ManagedFeedImage? {
 		let request = NSFetchRequest<ManagedFeedImage>(entityName: entity().name!)
 		request.predicate = NSPredicate(format: "%K = %@", argumentArray: [#keyPath(ManagedFeedImage.url), url])
@@ -29,15 +35,24 @@ extension ManagedFeedImage {
 		request.fetchLimit = 1
 		return try context.fetch(request).first
 	}
-	
+
 	static func images(from feed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
-		return NSOrderedSet(array: feed.map { local in
+		let images = NSOrderedSet(array: feed.map { local in
 			let managed = ManagedFeedImage(context: context)
 			managed.id = local.id
 			managed.imageDescription = local.description
 			managed.location = local.location
 			managed.url = local.url
+			managed.data = context.userInfo[local.url] as? Data
 			return managed
 		})
+		context.userInfo.removeAllObjects()
+		return images
+	}
+
+	override func prepareForDeletion() {
+		super.prepareForDeletion()
+
+		managedObjectContext?.userInfo[url] = data
 	}
 }
